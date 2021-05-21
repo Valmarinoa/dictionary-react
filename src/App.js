c8e55c3513ec9f61484c50cefacaf0e926268e6a
Valmarinoa <valmarinocol@gmail.com>
2021-05-21 11:11:33 +0200

    created form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo_1.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="cotainer">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Opensource code by Valentina Marino
+        </footer>
+      </div>
     </div>
   );
 }
